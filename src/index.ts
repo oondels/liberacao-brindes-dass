db8b56bce1099842a39e26b62176a269ff8c664f
Hendrius Dass <hendriusfelix.dev@gmail.com>
2026-02-13 09:40:46 -0300

    feat: adiciona centralizacao de tratamento de erros e error customizado

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import apiRouter from "./routes/index";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 import { AppDataSource } from "./config/db";
 import { config } from "./config/dotenv"
+import { CustomError } from "./types/CustomError";
 
 const bootstrap = async () => {
   await AppDataSource.initialize()
@@ -18,6 +19,24 @@ const bootstrap = async () => {
   app.get("/", (req: Request, res: Response) => res.send("App liberacao de brindes esta rodando!"));
 
   app.use("/api", apiRouter);
+
+  app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+
+  const statusCode = error instanceof CustomError ? error.statusCode : 500;
+  const message = error.message || "Erro interno no servidor.";
+  const details = error.details || null;
+
+  console.error(`Erro no método ${req.method} em ${req.originalUrl} - ${message}`);
+  if (details) {
+    console.error("Detalhes do erro: ", details);
+  }
+
+  res.status(statusCode).json({
+    message: message + " Contate a equipe de automação!",
+    ...(process.env.DEV_ENV === "development" && details && { details }),
+  });
+  return;
+});
 
   app.listen(port, () => console.log(`Application is running at http://localhost:${port}`));
 }
